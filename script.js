43824be590801ae7c6837998dbcc3cb97fed8d03
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-06-06 20:29:14 +0800

    Change color of button and div container

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ body.insertBefore(container, jsScript);
 const button = document.createElement("button");
 body.insertBefore(button, container);
 button.textContent = "Click me";
-
+button.setAttribute("style", "padding: 10px 25px; border-radius: 5px; border: 2px solid rgb(95, 95, 124); background-color: rgb(197, 197, 219)")
 
 
 container.setAttribute("style",
@@ -17,14 +17,14 @@ for (let i = 0; i <= 255; i++) {
   squareDiv = document.createElement("div");
   squareDiv.classList.add("square");
   container.appendChild(squareDiv);
-  squareDiv.setAttribute("style", "height: 25px; width: 25px; background-color: rgb(131, 131, 155);");
+  squareDiv.setAttribute("style", "height: 25px; width: 25px; background-color: rgb(197, 197, 219);");
 
   squareDiv.addEventListener("mouseenter", (event) => {
     event.target.style.backgroundColor = "rgb(95, 95, 124)";
   });
 
   squareDiv.addEventListener("mouseleave", (event) => {
-    event.target.style.backgroundColor = "rgb(131, 131, 155)";
+    event.target.style.backgroundColor = "rgb(197, 197, 219)";
   });
 };
 
