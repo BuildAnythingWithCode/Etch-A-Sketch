9a7beabda17e943d0f1c0356e34de9f2d4faebad
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-06-07 17:50:42 +0800

    Integrate button functionality to working status
    Clicking the button once and entering a number
    removes the original container and creates a
    new one, the new one having the same height and
    width as before, 400px. It effectively changes
    PPI. Clicking the button a second time throws an
    error.

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-body = document.querySelector("body");
-jsScript = document.querySelector("script");
-container = document.createElement("div");
-container.classList.add("container");
-body.insertBefore(container, jsScript);
-
+const body = document.querySelector("body");
+const jsScript = document.querySelector("script");
+const container = document.createElement("div");
 const button = document.createElement("button");
+body.insertBefore(container, jsScript);
 body.insertBefore(button, container);
 button.textContent = "Let's Get Set Up";
 button.setAttribute("style", "padding: 10px 25px; border-radius: 5px; border: 2px solid rgb(95, 95, 124); background-color: rgb(197, 197, 219)")
 
-button.addEventListener("click", () => {
-  let numOfSquares = Number(prompt("How many squares would you like per side?"));
-  if (numOfSquares >= 1 && 
-    numOfSquares <= 100 && 
-    typeof numOfSquares === 'number' && 
-    Number.isInteger(numOfSquares) === true) {
-    container.style.width = (numOfSquares * 25) + `px`;
-  } else {
-    alert("You must enter an integer between 1 and 100.");
-  }
-});
+body.setAttribute("style",
+  "max-width: 100vw; height: 100vh; display: flex; justify-content: space-around; align-items: center; flex-direction: column; overflow: hidden; background-color: rgb(36, 36, 43);");
 
 container.setAttribute("style",
-  "display: flex; align-items: center; justify-content: center; width: 400px; flex-wrap: wrap;");
+  "display: flex; align-items: center; justify-content: center; width: 400px; height: 400px; flex-wrap: wrap; overflow: hidden;");
 
 for (let i = 0; i <= 255; i++) {
-  squareDiv = document.createElement("div");
-  squareDiv.classList.add("square");
+  const squareDiv = document.createElement("div");
   container.appendChild(squareDiv);
   squareDiv.setAttribute("style", "height: 25px; width: 25px; background-color: rgb(197, 197, 219);");
 
@@ -37,5 +25,51 @@ for (let i = 0; i <= 255; i++) {
   squareDiv.addEventListener("mouseleave", (event) => {
     event.target.style.backgroundColor = "rgb(197, 197, 219)";
   });
+
+  squareDiv.addEventListener("mousedown", (event) => {
+    event.target.style.backgroundColor = "red";
+  });
 };
 
+button.addEventListener("click", () => {
+
+  let numOfSquares = Number(prompt("How many squares would you like per side?"));
+
+  if (numOfSquares >= 1 &&
+    numOfSquares <= 100 &&
+    typeof numOfSquares === 'number' &&
+    Number.isInteger(numOfSquares)) {
+
+    body.removeChild(container);
+
+    const containerNew = document.createElement("div");
+    body.insertBefore(containerNew, jsScript);
+
+    containerNew.setAttribute("style",
+      "display: flex; align-items: center; justify-content: center; width: 400px; height: 400px; flex-wrap: wrap; overflow: hidden;");
+
+    for (let i = 0; i <= (numOfSquares * numOfSquares); i++) {
+
+      const squareDivNew = document.createElement("div");
+      containerNew.appendChild(squareDivNew);
+      squareDivNew.setAttribute("style", "background-color: rgb(197, 197, 219);");
+
+      squareDivNew.style.width = (400 / numOfSquares) + `px`;
+      squareDivNew.style.height = (400 / numOfSquares) + `px`;
+
+      squareDivNew.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "rgb(95, 95, 124)";
+      });
+
+      squareDivNew.addEventListener("mouseleave", (event) => {
+        event.target.style.backgroundColor = "rgb(197, 197, 219)";
+      });
+
+      squareDivNew.addEventListener("mousedown", (event) => {
+        event.target.style.backgroundColor = "blue";
+      });
+    };
+  } else {
+    alert("You must enter an integer between 1 and 100.");
+  }
+});
