fbe5d59417027da882d52775e40dbc28da3dec73
BuildAnythingWithCode <168317593+BuildAnythingWithCode@users.noreply.github.com>
2024-06-07 20:19:27 +0800

    Add opacity increase on click, delete style.css

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Etch-A-Sketch
 
-This is an etch-a-sketch project made with HTML, CSS and JavaScript.
+This is an etch-a-sketch project made with only boilerplate HTML and JavaScript. Most DOM elements are created and manipulated using JavaScript.
--- script.js
+++ script.js
@@ -4,24 +4,19 @@ const container = document.createElement("div");
 const button = document.createElement("button");
 body.insertBefore(container, jsScript);
 body.insertBefore(button, container);
-button.textContent = "Change Canvas";
+button.textContent = "Resize Canvas";
 button.setAttribute("style", "padding: 10px 25px; border-radius: 5px; border: 2px solid rgb(95, 95, 124); background-color: rgb(197, 197, 219)");
-
 body.setAttribute("style",
-  "max-width: 100vw; height: 100vh; display: flex; justify-content: space-around; align-items: center; flex-direction: column; background-color: rgb(36, 36, 43);");
-
+  "max-width: 100vw; height: 100vh; display: flex; justify-content: space-around; align-items: center; flex-direction: column; background-color: rgb(36, 36, 43); padding: 0; margin: 0; box-sizing: border-box;");
 container.setAttribute("style",
   "display: flex; align-items: center; justify-content: center; width: 400px; height: 400px; flex-wrap: wrap;");
-
 for (let i = 0; i <= 255; i++) {
   const squareDiv = document.createElement("div");
   container.appendChild(squareDiv);
   squareDiv.setAttribute("style", "height: 25px; width: 25px; background-color: rgb(197, 197, 219);");
-
   squareDiv.addEventListener("mouseenter", (event) => {
     event.target.style.backgroundColor = "rgb(95, 95, 124)";
   });
-
   squareDiv.addEventListener("mouseleave", (event) => {
     function randomColor() {
       let red = Math.floor(Math.random() * 256);
@@ -31,43 +26,32 @@ for (let i = 0; i <= 255; i++) {
     };
     event.target.style.backgroundColor = randomColor();
   });
-
   let opacityValue = 1;
   squareDiv.addEventListener("mousedown", (event) => {
-    event.target.style.opacity = opacityValue - 0.1;
+    opacityValue -= 0.1;
+    event.target.style.opacity = opacityValue;
   });
 };
-
 button.addEventListener("click", () => {
-
   let numOfSquares = Number(prompt("How many squares would you like per side?"));
-
   if (numOfSquares >= 1 &&
     numOfSquares <= 100 &&
     typeof numOfSquares === 'number' &&
     Number.isInteger(numOfSquares)) {
-
     body.removeChild(container);
-
     const containerNew = document.createElement("div");
     body.insertBefore(containerNew, jsScript);
-
     containerNew.setAttribute("style",
       "display: flex; align-items: center; justify-content: center; width: 400px; height: 400px; flex-wrap: wrap;");
-
     for (let i = 0; i <= (numOfSquares * numOfSquares) - 1; i++) {
-
       const squareDivNew = document.createElement("div");
       containerNew.appendChild(squareDivNew);
       squareDivNew.setAttribute("style", "background-color: rgb(197, 197, 219);");
-
       squareDivNew.style.width = (400 / numOfSquares) + `px`;
       squareDivNew.style.height = (400 / numOfSquares) + `px`;
-
       squareDivNew.addEventListener("mouseenter", (event) => {
         event.target.style.backgroundColor = "rgb(95, 95, 124)";
       });
-
       squareDivNew.addEventListener("mouseleave", (event) => {
         function randomColor() {
           let red = Math.floor(Math.random() * 256);
@@ -77,10 +61,10 @@ button.addEventListener("click", () => {
         };
         event.target.style.backgroundColor = randomColor();
       });
-
       let opacityValue = 1;
       squareDivNew.addEventListener("mousedown", (event) => {
-        event.target.style.opacity = opacityValue - 0.1;
+        opacityValue -= 0.1;
+        event.target.style.opacity = opacityValue;
       });
     };
   } else {
